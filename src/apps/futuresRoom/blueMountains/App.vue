6887abfe4b7ff7c07edd9743486bcd21f3c43d4a
hartswf0 <watson.hartsoe@gmail.com>
2022-02-17 20:57:16 -0500

    blueMountain Al Test
    does this commit show up in hubs?

--- src/apps/futuresRoom/blueMountains/App.vue
+++ src/apps/futuresRoom/blueMountains/App.vue
@@ -2,7 +2,7 @@
   <div> 
   <div id="room" class="darkwall">
   <div class="spacer">
-  <Title msg="Book: Reali" />
+  <Title msg="Test with Al" />
 	<div class="squareoff" style="width:380px">Published by <a href="https://mitpress.mit.edu/books/reality-media">MIT Press</a></div>
   <br>
   <div class="oblique squareoff">Georgraphy of virtual environments</div> 
